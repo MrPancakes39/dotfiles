8a48f9e2f97f789d681a382a8884c771a1962fb1
Salman Hasan <41960961+MrPancakes39@users.noreply.github.com>
2022-04-18 13:36:05 +0300

    Adding fish shell configs

---
+++ .config/fish/config.fish
@@ -0,0 +1,71 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+
+    ### ADDING TO THE PATH
+    # First line removes the path; second line sets it.  Without the first line,
+    # your path gets massive and fish becomes very slow.
+    set -e fish_user_paths
+    set -U fish_user_paths $HOME/.local/bin $HOME/Applications $HOME/Applications/bin $fish_user_paths
+
+    ### EXPORT ###
+    set fish_greeting                   # Supresses fish's intro message
+    set TERM "kitty"                    # Sets the terminal type
+    set EDITOR "nvim"                   # $EDITOR use Neovim
+    set VISUAL "code"                   # $VISUAL use VSCode
+
+    ### FUNCTIONS ###
+    function mkcd
+        mkdir -p $argv
+        and cd $argv[-1]
+    end
+
+    ### ALIASES ### 
+
+    # editing config file
+    alias fshconf="nvim ~/.config/fish/config.fish"
+    alias reload="source ~/.config/fish/config.fish"
+
+    # file related aliases
+    alias cp="cp -i"
+    alias mv="mv -i"
+    alias rm="rm -i"
+    alias del="trash"
+
+    # Changing "ls" to "exa"
+    alias ls="exa --color=always --group-directories-first"
+    alias ll="exa -lh --color=always --group-directories-first"
+    alias lt="exa -aT --color=always --group-directories-first"
+    alias l.="exa -a | egrep '^\.'"
+
+    # Colorize grep output (good for log files)
+    alias grep="grep --color=auto"
+    alias egrep="egrep --color=auto"
+    alias fgrep="fgrep --color=auto"
+
+    # adding flags
+    alias df="df -h"                                # human-readable sizes
+    alias free="free -m"                            # show sizes in MB
+
+    # utils
+    alias cls="clear"                               # adds clear cmd from Windows
+    alias psa="ps auxf"                             # shows all processes
+    alias jctl="journalctl -p 3 -xb"                # get error messages from journalctl
+    alias cleanup="sudo pacman -Rns (pacman -Qtdq)" # remove orphaned packages
+    alias dir-open="xdg-open ."                     # nice alias to open folder
+
+    # get fastest mirrors
+    alias mirror="sudo reflector -f 30 -l 30 --number 10 --verbose --save /etc/pacman.d/mirrorlist"
+    alias mirrord="sudo reflector --latest 50 --number 20 --sort delay --save /etc/pacman.d/mirrorlist"
+    alias mirrors="sudo reflector --latest 50 --number 20 --sort score --save /etc/pacman.d/mirrorlist"
+    alias mirrora="sudo reflector --latest 50 --number 20 --sort age --save /etc/pacman.d/mirrorlist"
+
+    # bare git repo alias for dotfiles
+    # https://www.atlassian.com/git/tutorials/dotfiles
+    alias config="/usr/bin/git --git-dir=$HOME/dotfiles --work-tree=$HOME"
+
+    # termbin
+    alias tb="nc termbin.com 9999"
+
+    ### RANDOM COLOR SCRIPT ###
+    # colorscript random
+end
